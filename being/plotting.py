46ab1ee5e473b3beaa11c9ecaa6fb870df06cf04
atheler <atheler@users.noreply.github.com>
2020-12-29 10:40:17 +0100

    plot_spline()

--- being/plotting.py
+++ being/plotting.py
@@ -1,6 +1,7 @@
 """Plotting util."""
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import PPoly
 
 from being.constants import ONE_D
 
@@ -32,3 +33,43 @@ def plot_trajectory(t, trajectory, *args, ax=None, labelit=False, **kwargs):
 
     if labelit:
         ax.legend()
+
+
+def sample_trajectory(spline: PPoly, nSamples: int = 100, rett: bool = False):
+    """Sample trajectory values from spline. Optionally also return sample
+    times.
+
+    Args:
+        spline: Spline to sample.
+
+    Kwargs:
+        nSamples: Number of samples
+        rett: If to return sample times as well.
+
+    Returns:
+        Trajectory.
+    """
+    start, end = spline.x[[0, -1]]
+    t = np.linspace(start, end, nSamples)
+    order = spline.c.shape[0]
+    trajectory = np.array([
+        spline(t, nu) for nu in range(order)
+    ])
+    if rett:
+        return t, trajectory.T
+
+    return trajectory.T
+
+
+def plot_spline(spline: PPoly, nSamples: int = 100, **kwargs):
+    """Plot trajectory of spline.
+
+    Args:
+        spline: Spline to plot.
+
+    Kwargs:
+        nSamples: Number of samples
+        **kwargs -> plot_trajectory()
+    """
+    t, trajectory = sample_trajectory(spline, nSamples, rett=True)
+    plot_trajectory(t, trajectory, **kwargs)
