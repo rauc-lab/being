03ee8c08d81df966d0786aa70db1708f2458b1c6
sja <silvan.januth@artorg.unibe.ch>
2021-01-19 09:14:42 +0100

    added dummy rest api

--- being/server.py
+++ being/server.py
@@ -1,6 +1,7 @@
 """Web server backend."""
 import asyncio
 import logging
+import json
 
 import aiohttp
 from aiohttp import web
@@ -57,11 +58,123 @@ def init_web_server():
     app.router.add_get('/', file_response_handler('static/index.html'))
     app.router.add_static(prefix='/static', path='./static', show_index=True)
 
-    # Routes
-    # GET -> app.router.add_get(url, handler)
-    # PUT -> app.router.add_put(url, handler)
-    # POST -> app.router.add_post(url, handler)
-    # ...
+    routes = web.RouteTableDef()
+    apiPrefix = '/api/'
+
+    @routes.get(apiPrefix + 'hello')
+    async def say_hello(request: web.Request):
+        if 'name' in request.query:
+            return web.json_response(
+                f"Hello {request.query['name']}"
+            )
+        else:
+            return web.json_response("Hello world")
+
+    @routes.get(apiPrefix + 'graph')
+    async def get_graph(request: web.Request):
+        raise aiohttp.web.HTTPNotImplemented()
+
+    @routes.get(apiPrefix + 'blocks')
+    async def get_blocks(request: web.Request):
+        if 'type' in request.query:
+            raise aiohttp.web.HTTPNotImplemented()
+        else:
+            raise aiohttp.web.HTTPNotImplemented()
+
+    @routes.get(apiPrefix + 'blocks/{id}')
+    async def get_block(request: web.Request):
+        return web.json_response(f"TODO: return {request.match_info['id']}")
+
+    @routes.put(apiPrefix + 'blocks/{id}')
+    async def update_block(request: web.Request):
+        try:
+            data = await request.json()
+            # TODO : update block
+            return await get_block(request)
+        except json.decoder.JSONDecodeError:
+            raise aiohttp.web.HTTPBadRequest()
+
+    @routes.get(apiPrefix + 'connections')
+    async def get_connections(request: web.Request):
+        raise aiohttp.web.HTTPNotImplemented()
+
+    @routes.get(apiPrefix + 'state')
+    async def get_state(request: web.Request):
+        return web.json_response('stopped')
+
+    @routes.put(apiPrefix + 'state')
+    async def set_state(request: web.Request):
+        try:
+            reqState = await request.json()
+            reqState = reqState.upper()
+
+            # TODO : set states
+            if reqState == 'RUN':
+                try:
+                    print(f"set new state to {reqState}")
+                    return web.json_response("RUNNING")
+                except Exception as e:
+                    return aiohttp.web.HTTPInternalServerError(reson=e)
+
+            elif reqState == 'PAUSE':
+                try:
+                    print(f"set new state to {reqState}")
+                    return web.json_response("PAUSED")
+                except Exception as e:
+                    return aiohttp.web.HTTPInternalServerError(reson=e)
+
+            elif reqState == 'STOP':
+                try:
+                    print(f"set new state to {reqState}")
+                    return web.json_response("STOPPED")
+                except Exception as e:
+                    return aiohttp.web.HTTPInternalServerError(reson=e)
+
+            else:
+                raise aiohttp.web.HTTPBadRequest()
+
+        except json.decoder.JSONDecodeError:
+            raise aiohttp.web.HTTPBadRequest()
+
+    @routes.get(apiPrefix + 'motions')
+    async def get_motions(request: web.Request):
+        return web.json_response(
+            {
+                "demo.json": "",
+                "demo2.json": "Test",
+            },
+        )
+
+    @routes.post(apiPrefix + 'motions')
+    async def post_motion(request: web.Request):
+        try:
+            data = await request.post()
+            filename = list(data.keys())[0]
+            if filename:
+                # TODO: create new motion object & sanity check
+                return web.json_response(data)
+            else:
+                return aiohttp.web.HTTPBadRequest()
+        except Exception as e:
+            return aiohttp.web.HTTPInternalServerError(reson=e)
+
+    @routes.get(apiPrefix + 'motions/{name}')
+    async def get_motion(request: web.Request):
+        raise aiohttp.web.HTTPNotImplemented()
+
+    @routes.put(apiPrefix + 'motions/{name}')
+    async def put_motion(request: web.Request):
+        raise aiohttp.web.HTTPNotImplemented()
+
+    @routes.delete(apiPrefix + 'motions/{name}')
+    async def delete_motion(request: web.Request):
+        raise aiohttp.web.HTTPNotImplemented()
+
+    @routes.get(apiPrefix + 'block-network/state')
+    async def get_block_network_state(request: web.Request):
+        raise aiohttp.web.HTTPNotImplemented()
+
+    app.add_routes(routes)
 
     # Setup websocket
     app.router.add_get('/data-stream', handle_web_socket)
