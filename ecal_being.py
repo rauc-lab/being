1bf2499263f318eb2c533dcbf69b9f19c2751ed2
atheler <atheler@users.noreply.github.com>
2021-04-22 16:33:17 +0200

    Updated main program for 1x or 2x motors

--- ecal_being.py
+++ ecal_being.py
@@ -8,18 +8,44 @@ from being.resources import manage_resources
 from being.sensors import SensorGpio
 
 
+NODE_IDS = [1, 2]
+"""Motor ids to use."""
+
+
+def add_motor(mp, motor):
+    """Connect motor to free position output from motion player. Create new
+    position output if necessary.
+    """
+    for output in mp.positionOutputs:
+        if not output.connected:
+            output.connect(motor.input)
+            return
+
+    mp.add_position_output()
+    mp.positionOutputs[-1].connect(motor.input)
+
+
 with manage_resources():
     # Initialize being blocks
     sensor = SensorGpio(channel=6)
     behavior = Behavior.from_config('behavior.json')
     mp = MotionPlayer()
-    motor0 = Motor(nodeId=9, length=0.100)
-    motor1 = Motor(nodeId=8, length=0.100)
+    motors = []
+    for nodeId in NODE_IDS:
+        try:
+            mot = Motor(nodeId, length=0.100)
+            motors.append(mot)
+        except RuntimeError:
+            pass
+
+    if len(motors) == 0:
+        raise RuntimeError('Found no motors!')
 
     # Make block connections
     sensor.output.connect(behavior.input)
     behavior.associate(mp)
-    mp.add_position_output()
-    mp.outputs[0].connect(motor0.input)
-    mp.outputs[1].connect(motor1.input)
+
+    for mot in motors:
+        add_motor(mp, mot)
+
     awake(behavior)
