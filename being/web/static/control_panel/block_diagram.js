e2b887bdd8793bef4f10d7aaf07a6bca135c6477
atheler <atheler@users.noreply.github.com>
2021-07-22 17:41:36 +0200

    Block diagram with curvy lines, doc

--- being/web/static/control_panel/block_diagram.js
+++ being/web/static/control_panel/block_diagram.js
@@ -5,22 +5,42 @@
 
 import {deep_copy, remove_all_children} from "/static/js/utils.js";
 import {create_element, setattr} from "/static/js/svg.js";
-import {BBox} from "/static/js/bbox.js";
-import {subtract_arrays} from "/static/js/array.js";
+import {subtract_arrays, add_arrays} from "/static/js/array.js";
 
 
-/*
- * From here: http://thenewcode.com/1068/Making-Arrows-in-SVG
+const STROKE_WIDTH = 2;
+const Direction = Object.freeze({
+    "NORTH": 0,
+    "WEST": 1,
+    "SOUTH": 2,
+    "EAST": 3,
+});
+
+/**
+ * Convert a pont object into a 2d array.
+ *
+ * @param {Obect} pt Point object.
+ * @returns 2d point array.
+ */
+function pt_to_array(pt) {
+    return [pt.x, pt.y];
+}
+
+
+/**
+ * Add marker definition (#arrowhead) to SVG.
+ * Taken from here: http://thenewcode.com/1068/Making-Arrows-in-SVG
+ *
+ * @param {?} svg SVG HTML element.
  */
 function add_arrow_marker_definition(svg) {
-
     const defs = create_element("defs");
     const marker = create_element("marker");
     setattr(marker, "id", "arrowhead");
     setattr(marker, "markerWidth", 10);
     setattr(marker, "markerHeight", 7);
     //setattr(marker, "refX", 10);  // Original arrow
-    setattr(marker, "refX", 7);
+    setattr(marker, "refX", 6.5);
     setattr(marker, "refY", 3.5);
     setattr(marker, "orient", "auto");
 
@@ -34,6 +54,13 @@ function add_arrow_marker_definition(svg) {
 }
 
 
+/**
+ * Draw a ELK layout node on a SVG. Also draw a text with the name.
+ *
+ * @param {?} svg SVG HTML element.
+ * @param {Object} child Node object from ELK layout graph.
+ * @returns SVG group element representing the block.
+ */
 function draw_block(svg, child) {
     const g = create_element("g");
     setattr(g, "transform", "translate(" + child.x + " " + child.y + ")")
@@ -43,7 +70,7 @@ function draw_block(svg, child) {
     setattr(rect, "width", child.width);
     setattr(rect, "height", child.height);
     setattr(rect, "stroke", "black");
-    setattr(rect, "stroke-width", 2);
+    setattr(rect, "stroke-width", STROKE_WIDTH);
     setattr(rect, "fill", "none");
     g.appendChild(rect);
 
@@ -59,6 +86,14 @@ function draw_block(svg, child) {
 }
 
 
+/**
+ * Draw a staight line on a SVG.
+ *
+ * @param {?} svg SVG HTML element.
+ * @param {Array} start 2d start point.
+ * @param {Array} end 2d end point.
+ * @returns SVG line element.
+ */
 function draw_line(svg, start, end) {
     const [x1, y1] = start;
     const [x2, y2] = end;
@@ -68,28 +103,92 @@ function draw_line(svg, start, end) {
     setattr(line, "x2", x2);
     setattr(line, "y2", y2);
     setattr(line, "stroke", "black");
-    setattr(line, "stroke-width", 2);
+    setattr(line, "stroke-width", STROKE_WIDTH);
     svg.appendChild(line);
     return line;
 }
 
 
-function pt_to_array(pt) {
-    return [pt.x, pt.y];
+/**
+ * Draw a curvy line on a SVG.
+ *
+ * @param {?} svg SVG HTML element.
+ * @param {Array} start 2d start point.
+ * @param {Direction} startDir Start direction.
+ * @param {Array} end 2d end point.
+ * @param {Direction} endDir End direction.
+ * @returns SVG line element.
+ */
+function draw_curvy_line(svg, start, startDir, end, endDir) {
+    const path = create_element("path");
+    svg.appendChild(path);
+
+    // Offset vectors for each 4x possible connection direction
+    const offsets = {};
+    const [dx, dy] = subtract_arrays(end, start);
+    offsets[Direction.NORTH] = [0, -dy];
+    offsets[Direction.EAST] = [dx, 0];
+    offsets[Direction.SOUTH] = [0, dy];
+    offsets[Direction.WEST] = [-dx, 0];
+
+    // Curvy cubic path
+    const cps = [
+        start,
+        add_arrays(start, offsets[startDir]),
+        add_arrays(end, offsets[endDir]),
+        end,
+    ];
+    const d = "M" + cps[0] + "C" + cps.slice(1).flat();
+    setattr(path, "d", d);
+
+    // Rest
+    setattr(path, "fill", "none");
+    setattr(path, "stroke", "black");
+    setattr(path, "stroke-width", STROKE_WIDTH);
+    return path;
 }
 
 
+/**
+ * Determine the connection direction on a ELK block node. Does the connection
+ * go NORTH, WEST, SOUTH or EAST?
+ *
+ * @param {Array} pt 2d point.
+ * @param {Object} child ELK layout graph node.
+ * @returns Connection direction.
+ */
+function determine_connection_direction(pt, child) {
+    // TODO: More sophisticated connection direction which also work for points
+    // which do not lay on child's edge.
+    const [x, y] = pt;
+    if (x === child.x) {
+        return Direction.WEST;
+    } else if (x === child.x + child.width) {
+        return Direction.EAST;
+    } else if (y === child.y) {
+        return Direction.NORTH;
+    } else if (y === child.y + child.height) {
+        return Direction.SOUTH;
+    }
+
+    throw "Could not determine connection direction. pt " + pt + " not laying on child!";
+}
+
+/**
+ * Draw block diagram on SVG.
+ *
+ * @param {?} svg SVG element to draw to.
+ * @param {Object} graph Block network graph.
+ */
 export async function draw_block_diagram(svg, graph) {
-    const elkGraph = deep_copy(graph);
-    elkGraph.children.forEach(child => {
+    const layout = deep_copy(graph);
+    layout.children.forEach(child => {
         child.width = 120;
         child.height = 60;
     })
-
-    // Layout graph
-    elkGraph.id = "root";
-    let elk = new ELK();
-    await elk.layout(elkGraph, {
+    layout.id = "root";
+    const elk = new ELK();
+    await elk.layout(layout, {
         layoutOptions: {
             "elk.algorithm": "layered",
             "elk.layered.spacing.nodeNodeBetweenLayers": 50,
@@ -99,28 +198,35 @@ export async function draw_block_diagram(svg, graph) {
         }
     });
 
-    const bbox = new BBox();
+    const lookup = {};
+    layout.children.forEach(child => {
+        lookup[child.id] = child;
+    });
     remove_all_children(svg);
     add_arrow_marker_definition(svg)
-    elkGraph.children.forEach(child => {
+    layout.children.forEach(child => {
         draw_block(svg, child);
-        bbox.expand_by_point([child.x, child.y]);
-        bbox.expand_by_point([child.x + child.width, child.y + child.height]);
     });
-    elkGraph.edges.forEach(edge => {
+    layout.edges.forEach((edge, nr) => {
         edge.sections.forEach(section => {
             const start = pt_to_array(section.startPoint);
             const end = pt_to_array(section.endPoint);
-            const line = draw_line(svg, start, end);
+
+            // Straight line
+            //const line = draw_line(svg, start, end);
+
+            // Curvy line
+            const incoming = lookup[section.incomingShape];
+            const outgoing = lookup[section.outgoingShape];
+            const startDir = determine_connection_direction(start, incoming);
+            const endDir = determine_connection_direction(end, outgoing);
+            const line = draw_curvy_line(svg, start, startDir, end, endDir);
+
             setattr(line, "marker-end", "url(#arrowhead)");
-            bbox.expand_by_points([start, end]);
         });
     });
 
-    console.log(bbox.ll, bbox.size)
-    const [x, y] = bbox.ll;
-    const [w, h] = bbox.size;
-    const m = 20;
-    const viewBox = [x-m, y-m, w+2*m, h+2*m].join(" ");
+    // Adjust viewBox
+    const viewBox = [layout.x, layout.y, layout.width, layout.height].join(" ");
     setattr(svg, "viewBox", viewBox);
 }
