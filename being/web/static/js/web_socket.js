a43bc3c2574b18ddd215b928e335a8a4d9f69659
atheler <atheler@users.noreply.github.com>
2021-07-24 10:43:01 +0200

    WebSocketCentral subscription class

--- being/web/static/js/web_socket.js
+++ being/web/static/js/web_socket.js
@@ -2,54 +2,62 @@
  * @module web_socket Small web socket wrapper.
  */
 import {MS} from "/static/js/constants.js";
-
-
-let NOTIFICATION_ID = 0;
+import {defaultdict} from "/static/js/utils.js";
 
 
 /**
- * Try to receive data from web socket connection and hand over to data to
- * function handler. Repeatedly try to reconnect if connection dead. Assumes
- * JSON data.
+ * Web socket central / relay. Try to receive data from web socket connection
+ * and hand over to data to function handler. Repeatedly try to reconnect if
+ * connection dead. Assumes Subscribe callbacks for message types. JSON data.
  *
  * @param url Web socket url to fetch data from.
- * @param callbacks Message type name -> Callback functions.
- * @param notificationCenter Notification instance to notify about web socket
- *     down or up and running again.
  * @param reconnectTimeout Reconnect timeout duration in seconds.
  */
-export function receive_from_websocket(url, callbacks, notificationCenter, reconnectTimeout=1.) {
-    const sock = new WebSocket(url);
-    sock.onopen = function() {
-        NOTIFICATION_ID = notificationCenter.notify_persistent(
-            "Connected to Being",
-            "success",
-            2,
-            NOTIFICATION_ID,
-        );
-    };
-    sock.onmessage = function(evt) {
-        let msg = JSON.parse(evt.data);
-        callbacks[msg.type].forEach(function(func) {
-            func(msg);
-        });
-    };
-    sock.onerror = function() {
-        sock.close();
-    };
-    sock.onclose = function() {
-        NOTIFICATION_ID = notificationCenter.notify_persistent(
-            "Being offline",
-            "error",
-            0,
-            NOTIFICATION_ID,
-        );
-        window.setTimeout(
-            receive_from_websocket,
-            reconnectTimeout * MS,
-            url,
-            callbacks,
-            notificationCenter,
-        );
-    };
+export class WebSocketCentral {
+    constructor(url, reconnectTimeout=1.) {
+        this.url = url;
+        this.reconnectTimeout = reconnectTimeout;
+        this.connected = false;
+        this.callbacks = { "open": [], "close": [], }
+        this.msgCallbacks = defaultdict(Array);
+    }
+
+    subscribe(event, callback) {
+        this.callbacks[event].push(callback);
+    }
+
+    subscribe_to_message(msgType, callback) {
+        this.msgCallbacks[msgType].push(callback);
+    }
+
+    connect() {
+        const sock = new WebSocket(this.url);
+        sock.onopen = evt => {
+            if (!this.connected) {
+                this.connected = true;
+                this.callbacks["open"].forEach(callback => {callback()})
+            }
+        };
+        sock.onmessage = evt => {
+            let msg = JSON.parse(evt.data);
+            this.msgCallbacks[msg.type].forEach(function(func) {
+                func(msg);
+            });
+        };
+        sock.onerror = evt => {
+            sock.close();
+        };
+        sock.onclose = evt => {
+            if (this.connected) {
+                this.connected = false;
+                this.callbacks["close"].forEach(callback => {callback()})
+            }
+
+            if (this.reconnectTimeout !== null) {
+                window.setTimeout(() => {
+                    this.connect()
+                }, this.reconnectTimeout * MS);
+            }
+        };
+    }
 }
