f3465ef2b54f28027a2b5dad46cafb2f15ab5c97
atheler <atheler@users.noreply.github.com>
2021-12-18 22:54:41 +0100

    Adjusted style for JS part as well

--- docs/conf.py
+++ docs/conf.py
@@ -66,6 +66,12 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'custom.css',
+]
+
 
 # -- Options extensions ------------------------------------------------------
 
