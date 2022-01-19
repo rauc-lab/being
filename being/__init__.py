2ee0819be57fcddfccdfae029f5edc65e651bd9d
atheler <atheler@users.noreply.github.com>
2022-01-19 13:06:38 +0100

    Fixes #4: Only patch PCAN interface if possible and necessary

--- being/__init__.py
+++ being/__init__.py
@@ -1,15 +1,27 @@
 """PATHOS being core."""
 import sys
 
-# PCAN on darwin patch
+
+# python-can PCAN on darwin patch. This is issue will get fixed with python-can
+# >= 4.0.0
 try:
     import can
     import canopen
-    if sys.platform.startswith('darwin'):
-        from being.can.pcan_darwin_patch import patch_pcan_on_darwin
+    from being.can.pcan_darwin_patch import (
+        is_pcan_lib_installed,
+        does_python_can_need_patching,
+        patch_pcan_on_darwin,
+    )
+
+    if (sys.platform.startswith('darwin')
+        and is_pcan_lib_installed()
+        and does_python_can_need_patching()
+    ):
         patch_pcan_on_darwin()
 except ImportError:
     pass
+
+
 
 __author__ = 'atheler'
 __version__ = '1.0.1'
--- being/can/pcan_darwin_patch.py
+++ being/can/pcan_darwin_patch.py
@@ -1,6 +1,10 @@
 """Temporary workaround for a small bug when using python-can on a Mac with a
 PCAN interface running with libPCBUSB == 0.9. More details can be found here:
 https://github.com/hardbyte/python-can/issues/957.
+
+Caution:
+    This issue seems to be fixed with python-can version >= 4.0.0. See
+    https://github.com/hardbyte/python-can/blob/develop/CHANGELOG.md
 """
 import re
 import sys
@@ -12,8 +16,24 @@ import canopen
 from can.interfaces import pcan
 
 
+PCAN_LIBRARY_NAME = 'libPCBUSB.dylib'
 PCAN_NONEBUS = c_ushort(0x00)
 PCAN_EXT_SOFTWARE_VERSION = c_ushort(0x86)
+
+
+def is_pcan_lib_installed() -> bool:
+    """Check if PCAN driver library is installed or not."""
+    try:
+        cdll.LoadLibrary(PCAN_LIBRARY_NAME)
+        return True
+    except OSError:
+        return False
+
+
+def does_python_can_need_patching() -> bool:
+    """Check python-can version number for issue."""
+    major = can.__version__.split('.', maxsplit=1)[0]
+    return int(major) < 4
 
 
 def patch_pcan_on_darwin():
@@ -27,20 +47,33 @@ def patch_pcan_on_darwin():
         ... # Import can & canopen before patching
         ... patch_pcan_on_darwin()
     """
-    if sys.platform.startswith('darwin'):
-        # Get version number from dylib
-        lib = cdll.LoadLibrary('libPCBUSB.dylib')
-        buf = create_string_buffer(256)
-        res = lib.CAN_GetValue(PCAN_NONEBUS, PCAN_EXT_SOFTWARE_VERSION, byref(buf), sizeof(buf))
-        if res:
-            raise RuntimeError('Could not get value from lib!')
-
-        ptr = b'.+version (\d+)\.(\d+)\.(\d+)\.\d+'
-        m = re.match(ptr, buf.value)
-        major, minor, micro = map(int, m.groups())
-        if minor >= 9:
-            for mod in [pcan.basic, pcan.pcan]:
-                mod.TPCANMsgMac = mod.TPCANMsg
-                mod.TPCANMsgFDMac = mod.TPCANMsgFD
-    else:
+    if not sys.platform.startswith('darwin'):
         warnings.warn('Not on a Mac. Nothing to patch')
+        return
+
+    if not is_pcan_lib_installed():
+        warnings.warn('PCAN driver %r seems not to be installed' % PCAN_LIBRARY_NAME)
+        return
+
+    if not does_python_can_need_patching():
+        warnings.warn('python-can >= 4.0.0 does not need patching')
+        return
+
+    # Get version number from dylib
+    lib = cdll.LoadLibrary(PCAN_LIBRARY_NAME)
+    buf = create_string_buffer(256)
+    res = lib.CAN_GetValue(PCAN_NONEBUS, PCAN_EXT_SOFTWARE_VERSION, byref(buf), sizeof(buf))
+    if res:
+        raise RuntimeError('Could not get value from lib!')
+
+    pattern = b'.+version (\d+)\.(\d+)\.(\d+)\.\d+'
+    m = re.match(pattern, buf.value)
+    major, minor, micro = map(int, m.groups())
+    if minor < 9:
+        version = b'.'.join(m.groups())
+        warnings.warn('Nothing to patch for PCAN driver version %s' % version.decode())
+        return
+
+    for mod in [pcan.basic, pcan.pcan]:
+        mod.TPCANMsgMac = mod.TPCANMsg
+        mod.TPCANMsgFDMac = mod.TPCANMsgFD
